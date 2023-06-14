3628d36ecc33d26b4182a39985a9d62830150791
Houssem Achouri <houssem@3dverse.com>
2023-06-14 11:21:34 -0400

    Add app files

---
+++ template.3dverse.js
@@ -0,0 +1,70 @@
+export default {
+  publicToken: "%PUBLIC_TOKEN%",
+  assets: {
+    SceneCarConfigurator: {
+      asset_id: "d427cd4c-05a4-41b3-856f-70fbc746f081",
+      asset_type: "scene",
+      path: "/Public",
+    },
+    SceneViper: {
+      asset_id: "c2fd241f-1f70-4979-a857-afec61cffcf3",
+      asset_type: "scene",
+      path: "/Public",
+    },
+    SceneMercedes: {
+      asset_id: "73a30be6-3734-414e-8f29-900527caf6c8",
+      asset_type: "scene",
+      path: "/Public",
+    },
+    MaterialMetallic: {
+      asset_id: "80588b72-5101-4aaf-892c-f787db4f570b",
+      asset_type: "material",
+      path: "/Public",
+    },
+    MaterialSolid: {
+      asset_id: "36c74e90-a250-4820-a862-78f9b91a5a7a",
+      asset_type: "material",
+      path: "/Public",
+    },
+    MaterialMatte: {
+      asset_id: "84b006e1-de3f-4f77-8c7a-0c771834aee0",
+      asset_type: "material",
+      path: "/Public",
+    },
+    MaterialViperPaint: {
+      asset_id: "3a6555de-ad73-4b83-9672-8cce408c0b59",
+      asset_type: "material",
+      path: "/Public",
+    },
+    MaterialViperHeadlights: {
+      asset_id: "8a182d62-a3ae-4d8c-92b2-5fb94e772fd6",
+      asset_type: "material",
+      path: "/Public",
+    },
+    MaterialViperRearlights: {
+      asset_id: "6f2dcebf-f618-46ab-8d3a-14cb825762e7",
+      asset_type: "material",
+      path: "/Public",
+    },
+    MaterialMercedesPaint: {
+      asset_id: "093d11f5-02d7-46e3-ab9d-3a6fe75e354b",
+      asset_type: "material",
+      path: "/Public",
+    },
+    MaterialMercedesHeadlights: {
+      asset_id: "6cd356b7-4505-46b1-a6ea-c04d1967e49d",
+      asset_type: "material",
+      path: "/Public",
+    },
+    MaterialMercedesRearlights: {
+      asset_id: "1fe26ce1-5736-45d6-a76a-d3a0a6f1aa03",
+      asset_type: "material",
+      path: "/Public",
+    },
+    AnimationSequenceRotation: {
+      asset_id: "e7609135-dc4c-41ca-95ff-9dcbf86452d0",
+      asset_type: "animation_sequence",
+      path: "/Public",
+    }
+  },
+};
